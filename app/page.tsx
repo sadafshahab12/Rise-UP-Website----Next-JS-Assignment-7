0c722114ba028a866d43ae8a8018a4eb46cdfb03
sadafshahab12~ <sadafshahab123@yahoo.com>
2024-10-28 03:15:25 +0500

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Features from "@/components/Features";
-import Footer from "@/components/Footer";
 import GetinTouch from "@/components/GetinTouch";
 import Hero from "@/components/Hero";
 import JoinMission from "@/components/JoinMission";
 import Mission from "@/components/Mission";
-import Navbar from "@/components/Navbar";
 import Partners from "@/components/Partners";
 import SuccessStories from "@/components/SuccessStories";
 export default function Home() {
